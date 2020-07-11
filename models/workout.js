9d4e2c46f3b46f1aa46eb5145c51bf3cc6b94aa0
Samuel Trahan <samtrahan91@gmail.com>
2020-07-11 12:29:12 -0500

    workout model

---
+++ models/workout.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    genre: {
+        type: String,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Workouts', workoutSchema);
